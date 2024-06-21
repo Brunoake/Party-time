c48b2ab217717d1bfee26c226c94a9a92290266c
Brunoake <brunopachecogre@gmail.com>
2024-06-21 11:18:57 -0300

    .

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -1,23 +1,26 @@
 const router = require("express").Router()
 
-const serviceController = require("../controllers/serviceController")
+const serviceController = require("../controllers/serviceController");
 
 router
 .route("/services")
-.post((req, res) => serviceController.create(req, res))
+.post((req, res) => serviceController.create(req, res));
 
 router
 .route("/services")
-.get((req, res) => serviceController.getAll(req, res))
+.get((req, res) => serviceController.getAll(req, res));
 
 router
 .route("/services/:id")
-.get((req, res) => serviceController.get(req, res))
+.get((req, res) => serviceController.get(req, res));
 
 router
 .route("/services/:id")
-.delete((req, res) => serviceController.delete(req, res))
+.delete((req, res) => serviceController.delete(req, res));
 
+router
+.route("/services/:id")
+.put((req, res) => serviceController.update(req, res));
 
 
 module.exports = router
